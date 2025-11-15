7275f1e3bd258e789282b4e314c540178ca06f08
felipemths12 <felipe.matheussms@gmail.com>
2025-11-15 16:52:26 -0300

    feat: add ConsultaController logic

--- src/main/java/com/veterinaria/supervet/controller/ConsultaController.java
+++ src/main/java/com/veterinaria/supervet/controller/ConsultaController.java
@@ -1,4 +1,39 @@
 package com.veterinaria.supervet.controller;
 
+import com.veterinaria.supervet.dto.consulta.DadosAgendamentoConsulta;
+import com.veterinaria.supervet.dto.consulta.DadosDetalhamentoConsulta;
+import com.veterinaria.supervet.service.ConsultaService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/consultas")
 public class ConsultaController {
+
+    @Autowired
+    private ConsultaService consultaService;
+
+    @PostMapping
+    public ResponseEntity<DadosDetalhamentoConsulta> agendarConsulta(@RequestBody @Valid DadosAgendamentoConsulta dto) {
+        DadosDetalhamentoConsulta consultaAgendada = consultaService.agendar(dto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(consultaAgendada);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DadosDetalhamentoConsulta>> listarTodasAsConsultas() {
+        return ResponseEntity.ok(consultaService.listarTodasConsultas());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> cancelarConsulta(@PathVariable Long id) {
+        consultaService.cancelarConsulta(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
